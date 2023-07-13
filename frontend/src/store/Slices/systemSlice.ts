46181626c41c2015e9dad3e65d533836be60fa1f
joyshaheb <khondokoralam@gmail.com>
2023-07-13 09:20:49 +0600

    redux installed

---
+++ frontend/src/store/Slices/systemSlice.ts
@@ -0,0 +1,30 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// import type { PayloadAction } from "@reduxjs/toolkit";
+// import { ThemeTypesEnum } from "../../types/enum";
+// import { TailwindThemeType } from "../../types/types";
+
+// interface SystemState {
+//   mode: TailwindThemeType;
+// }
+
+// const initialState: SystemState = {
+//   mode: ThemeTypesEnum.DARK,
+// };
+
+// export const sysmtemSlice = createSlice({
+//   name: "system",
+//   initialState,
+//   reducers: {
+//     themeSwitch: (
+//       state: SystemState,
+//       action: PayloadAction<TailwindThemeType>
+//     ) => {
+//       state.mode = action.payload;
+//     },
+//     resetSystem: () => initialState,
+//   },
+// });
+
+// export const { themeSwitch, resetSystem } = sysmtemSlice.actions;
+
+export const x = 10;
