ffed213b6e6f5150649cc46cfe97b680dcb8aad8
joyshaheb <khondokoralam@gmail.com>
2023-07-24 10:32:37 +0600

    added pages and tailwind css

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import ErrorPage from "./ErrorPage";
+import UserProfile from "./UserProfile";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+
+export { Home, ErrorPage, UserProfile, Login, Signup };
