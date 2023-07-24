453b82f5bb699f6defb58bcf9ae4f27fe0f0905b
joyshaheb <khondokoralam@gmail.com>
2023-07-24 10:46:14 +0600

    added sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import { Home, ErrorPage, UserProfile } from "./pages";
+import { Home, ErrorPage, UserProfile, Login, Signup } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/SideBar/SideBar";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Sidebar>
     </BrowserRouter>
   );
 };
