0e332fc1885d823c0589be154f6708c85a88fe47
joyshaheb <khondokoralam@gmail.com>
2023-08-19 18:19:44 +0600

    server created

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-export const hello = "hi";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((res) => console.log("Connected to DB"))
+  .catch((err) => console.log(err, "Failed to connect to mongo DB"));
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
